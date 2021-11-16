8305295ed9b49c1ffc90b5a72ff5b6fa561d76f1
Puentes <joseluispuentes@hotmail.com>
2021-11-16 23:06:38 +0100

    ampliando

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.mde.entidades"},{"l":"es.mde.models"},{"l":"es.mde.repositorios"},{"l":"es.mde.rest"},{"l":"es.mde.servicios"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.mde.entidades"},{"l":"es.mde.models"},{"l":"es.mde.notificaciones"},{"l":"es.mde.repositorios"},{"l":"es.mde.rest"},{"l":"es.mde.servicios"}]
